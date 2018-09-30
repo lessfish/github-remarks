2b886ae243430515b792e790a4742b7d49852614
Muyunyun <328375795@qq.com>
2018-09-30 16:48:10 +0800

    perf: 修复了repo页不展示的bug并作了优化

--- content-scripts/repoDetail.js
+++ content-scripts/repoDetail.js
@@ -1,28 +1,27 @@
 // repo 详情页
 if (document.querySelector('.pagehead-actions')) {
-  const author = document.querySelector('#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div > h1 > span.author > a').innerHTML 
-  const repoName = document.querySelector('#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div > h1 > strong > a').innerHTML 
+  const author = document.querySelector('#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div > h1 > span.author > a').innerText
+  const repoName = document.querySelector('#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div > h1 > strong > a').innerText
   const repoKey = author + '/' + repoName
 
   chrome.storage.local.get('github-repo-remarks', res => {
     let obj = res['github-repo-remarks'] || {}
     const remark = obj[repoKey]
-
     // 已经有备注过
     if (remark) {
-      var el = document.querySelector('#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.js-repo-meta-container > div.repository-meta.mb-0.js-repo-meta-edit.js-details-container > div')
+      const el = document.querySelector('#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.mb-3 > div.f4 > span.text-gray-dark.mr-2')
 
-      var html = 
+      const html =
       `
       <div>
-        <div class="btn-group" style="margin-top:10px; margin-bottom:5px">
-          <a href="javascript:;" class="btn btn-sm" id="add-remarks-btn">编辑我的备注</a>
-          <a href="javascript:;" class="btn btn-sm" id="del-remarks-btn">清空这条备注</a>
+        <div class="btn-group" style="margin-top:10px; margin-bottom:-20px">
+          <span id="origin-remarks-content">${remark}</span>
+          <a href="javascript:;" class="btn btn-sm" id="add-remarks-btn">Edit Note</a>
         </div>
-        <p id="origin-remarks-content">${remark}</p>
-        <div style="display:none" id="edit-remarks-area">
-          <textarea class="form-control comment-form-textarea" id="remarks-content" style="margin-bottom:5px">${remark}</textarea>
-          <a href="javascript:;" class="btn btn-sm btn-primary" id="save-remarks-btn">保存</a>
+        <div style="display:none;margin-bottom:-20px;margin-top:26px" id="edit-remarks-area">
+          <textarea class="form-control comment-form-textarea" id="remarks-content">${remark}</textarea>
+          <a href="javascript:;" class="btn btn-sm btn-primary" id="save-remarks-btn" style="margin-top: 5px">Save</a>
+          <a href="javascript:;" class="btn btn-sm btn-danger" id="del-remarks-btn" style="margin-top: 5px">Delete</a>
         </div>
       </div>
       `
@@ -32,6 +31,7 @@ if (document.querySelector('.pagehead-actions')) {
       document.querySelector('#add-remarks-btn').onclick = () => {
         document.querySelector('#edit-remarks-area').style.display = 'block'
         document.querySelector('#origin-remarks-content').style.display = 'none'
+        document.querySelector('#add-remarks-btn').style.display = 'none'
         return false
       }
 
@@ -43,7 +43,6 @@ if (document.querySelector('.pagehead-actions')) {
           obj[repoKey] = content
 
           chrome.storage.local.set({'github-repo-remarks': obj}, () => {
-            console.log('saved!')
             location.reload()
           })
         })
@@ -59,7 +58,6 @@ if (document.querySelector('.pagehead-actions')) {
           delete obj[repoKey]
 
           chrome.storage.local.set({'github-repo-remarks': obj}, () => {
-            console.log('deleted!')
             location.reload()
           })
         })
@@ -67,17 +65,18 @@ if (document.querySelector('.pagehead-actions')) {
         return false
       }
     } else {
-      var el = document.querySelector('#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.js-repo-meta-container > div.repository-meta.mb-0.js-repo-meta-edit.js-details-container > div')
+      const el = document.querySelector('#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.mb-3 > div.f4 > span.text-gray-dark.mr-2')
 
-      var html = 
+      const html =
       `
       <div>
-        <div class="btn-group">
-          <a href="javascript:;" class="btn btn-sm" id="add-remarks-btn" style="margin-top:10px; margin-bottom:5px">增加我的备注</a>
+        <div class="btn-group" style="margin-top:10px; margin-bottom:-20px">
+          <a href="javascript:;" class="btn btn-sm" id="add-remarks-btn">Add Note</a>
         </div>
-        <div style="display:none" id="edit-remarks-area">
-          <textarea class="form-control comment-form-textarea" id="remarks-content" style="margin-bottom:5px"></textarea>
-          <a href="javascript:;" class="btn btn-sm btn-primary" id="save-remarks-btn">保存</a>
+        <div style="display:none;margin-bottom:-20px;margin-top:26px;" id="edit-remarks-area">
+          <textarea class="form-control comment-form-textarea" id="remarks-content"></textarea>
+          <a href="javascript:;" class="btn btn-sm btn-primary" id="save-remarks-btn" style="margin-top: 5px">Save</a>
+          <a href="javascript:;" class="btn btn-sm btn-danger" id="del-remarks-btn" style="margin-top: 5px">Delete</a>
         </div>
       </div>
       `
@@ -86,6 +85,7 @@ if (document.querySelector('.pagehead-actions')) {
 
       document.querySelector('#add-remarks-btn').onclick = () => {
         document.querySelector('#edit-remarks-area').style.display = 'block'
+        document.querySelector('#add-remarks-btn').style.display = 'none'
         return false
       }
 
@@ -97,7 +97,6 @@ if (document.querySelector('.pagehead-actions')) {
           obj[repoKey] = content
 
           chrome.storage.local.set({'github-repo-remarks': obj}, () => {
-            console.log('saved!')
             location.reload()
           })
         })
--- content-scripts/stars.js
+++ content-scripts/stars.js
@@ -10,38 +10,32 @@ function initStarsPage() {
       // 已经有备注过
       if (remark) {
         var el = item
-
-        var html = 
-        `
-        <div>
+        var html =
+        `<div>
           <div class="btn-group" style="margin-top:10px; margin-bottom:5px">
-            <a href="javascript:;" class="btn btn-sm add-remarks-btn">编辑我的备注</a>
-            <a href="javascript:;" class="btn btn-sm del-remarks-btn">清空这条备注</a>
+            <span class="origin-remarks-content">${remark}</span>
+            <a href="javascript:;" class="btn btn-sm add-remarks-btn">Edit Note</a>
           </div>
-          <p class="origin-remarks-content">${remark}</p>
-          <div style="display:none" class="edit-remarks-area">
+          <div style="display:none;margin-top:10px;" class="edit-remarks-area">
             <textarea class="form-control comment-form-textarea remarks-content" style="margin-bottom:5px">${remark}</textarea>
-            <a href="javascript:;" class="btn btn-sm btn-primary save-remarks-btn">保存</a>
+            <a href="javascript:;" class="btn btn-sm btn-primary save-remarks-btn">Save</a>
+            <a href="javascript:;" class="btn btn-sm btn-danger del-remarks-btn">Delete</a>
           </div>
-        </div>
-        `
+        </div>`
 
         el.insertAdjacentHTML('beforeend', html)
       } else {
         var el = item
-
-        var html = 
-        `
-        <div>
+        var html =
+        `<div>
           <div class="btn-group">
-            <a href="javascript:;" class="btn btn-sm add-remarks-btn" style="margin-top:10px; margin-bottom:5px">增加我的备注</a>
+            <a href="javascript:;" class="btn btn-sm add-remarks-btn" style="margin-top:10px; margin-bottom:5px">Add Note</a>
           </div>
-          <div style="display:none" class="edit-remarks-area">
+          <div style="display:none;margin-top:10px;" class="edit-remarks-area">
             <textarea class="form-control comment-form-textarea remarks-content" style="margin-bottom:5px"></textarea>
             <a href="javascript:;" class="btn btn-sm btn-primary save-remarks-btn">保存</a>
           </div>
-        </div>
-        `
+        </div>`
 
         el.insertAdjacentHTML('beforeend', html)
       }
@@ -50,9 +44,10 @@ function initStarsPage() {
     // 事件监听
     document.body.onclick = e => {
       let classList = e.target.classList
-      
+
       if (classList.contains('add-remarks-btn')) {
         e.target.parentNode.parentNode.querySelector('.edit-remarks-area').style.display = 'block'
+        e.target.parentNode.parentNode.querySelector('.btn-group').style.display = 'none'
         try {
           e.target.parentNode.parentNode.querySelector('.origin-remarks-content').style.display = 'none'
         } catch(err) {}
@@ -66,7 +61,6 @@ function initStarsPage() {
           obj[repoKey] = content
 
           chrome.storage.local.set({'github-repo-remarks': obj}, () => {
-            console.log('saved!')
             location.reload()
           })
         })
@@ -79,7 +73,6 @@ function initStarsPage() {
           delete obj[repoKey]
 
           chrome.storage.local.set({'github-repo-remarks': obj}, () => {
-            console.log('deleted!')
             location.reload()
           })
         })
@@ -90,6 +83,7 @@ function initStarsPage() {
 
     // 增加根据关键字匹配功能
     let listHtml = ''
+    let val = ''
     for (let k in obj) {
       let v = obj[k]
       listHtml += `
@@ -98,7 +92,7 @@ function initStarsPage() {
           <div class="jump-to-octicon js-jump-to-octicon mr-2 text-center"><svg height="16" width="16" class="octicon octicon-repo flex-shrink-0 js-jump-to-repo-octicon-template" title="Repository" viewBox="0 0 12 16" version="1.1" aria-hidden="true"><path fill-rule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg></div>
           <img class="avatar mr-2 flex-shrink-0 js-jump-to-suggestion-avatar d-none" alt="" src="" width="28" height="28">
           <div class="repos jump-to-suggestion-name js-jump-to-suggestion-name flex-auto overflow-hidden no-wrap css-truncate css-truncate-target">${k}</div>
-          <div class="intos" style="display:none">${v}</div>
+          <div class="intos">${v}</div>
 
           <div class="border rounded-1 flex-shrink-0 bg-gray px-1 text-gray-light ml-1 f6 d-none d-on-nav-focus js-jump-to-badge-search">
             Search
@@ -113,7 +107,7 @@ function initStarsPage() {
       </li>
       `
     }
-    
+
     let html = `
     <div class="header-search js-site-search position-relative" role="search" style="margin-top:10px">
       <div class="position-relative">
@@ -126,43 +120,36 @@ function initStarsPage() {
               </ul>
             </div>
           </label>
-        </form>  
+        </form>
       </div>
-  </div>
-    ` 
+    </div>
+    `
 
     document.querySelector('#js-pjax-container > div > div.col-9.float-left.pl-2 > div.position-relative > div.TableObject.border-bottom.border-gray-dark.py-3').insertAdjacentHTML('afterend', html)
 
-    var monkeyList = new List('repo-list', { 
+    var monkeyList = new List('repo-list', {
       valueNames: ['repos', 'intos']
     })
 
     // 先隐藏，有输入的时候再 list 出来
-    $('.fuzzy-search').on('input', e => {
-      let val = e.target.value.trim()
+    const fuzzySearch = document.getElementsByClassName('fuzzy-search')[0]
+    const listContainer = document.getElementsByClassName('js-jump-to-suggestions-container')[1]
+    fuzzySearch.addEventListener('input', e => {
+      val = e.target.value.trim()
       if (val) {
-        $('.js-jump-to-suggestions-container').eq(1).css('display', 'block')
+        listContainer.style.display = 'block'
       } else {
-        $('.js-jump-to-suggestions-container').eq(1).css('display', 'none')
+        listContainer.style.display = 'none'
       }
     })
   })
 }
 
-$(document).ready(() => {
-  let url = location.href 
+window.onload = function() {
+  let url = location.href
   let p = /.*\/\/github.com\/.*\?tab=stars.*/
 
   if (p.test(url)) {
     initStarsPage()
   }
-})
-
-$(document).on('pjax:complete', function() {
-  let url = location.href 
-  let p = /.*\/\/github.com\/.*\?tab=stars.*/
-
-  if (p.test(url)) {
-    initStarsPage()
-  }
-})
+}
